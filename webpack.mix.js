89a1d7c876357d3f40224c2a47a57179e5d6f661
Iman Erfani <iman.erfani@gmail.com>
2021-03-20 02:12:14 +0330

    Add laravel mix configurations

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,20 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.styles([
+    'node_modules/bootstrap/dist/css/bootstrap.min.css',
+    //'resources/lib/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
+    //'resources/lib/sweetalert2/sweetalert2.min.css',
+    //'resources/lib/summernote/summernote-bs4.min.css'
+],'public/css/app.css')
+    .version();
+
+mix.scripts([
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/popperjs/dist/umd/popper.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'node_modules/fortawesome/fontawesome-free/js/all.min.js',
+    //'resources/lib/sweetalert2/sweetalert2.min.js',
+    //'resources/lib/summernote/summernote-bs4.min.js'
+],'public/js/app.js')
+    .version();
