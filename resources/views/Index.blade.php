e938f4eba7901f16315ed90d52f5465007f110d8
Iman Erfani <iman.erfani@gmail.com>
2021-04-03 18:33:46 +0430

    Add react to project

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -6,23 +6,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="{{ mix('/css/website/app.css') }}" rel="stylesheet" type="text/css">
+    
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
- <h1>
-     <i class="fas fa-air-freshener"></i>
- </h1>
- <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div id="root">
+
     </div>
-  </div>
+ 
 
-
-    <script src="{{ mix('/js/website/app.js') }}"></script>
+    <script src="{{ mix('/js/app.js') }}"></script>
     
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,48 +6,12 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
- mix.styles([
-    'node_modules/bootstrap/dist/css/bootstrap.min.css',
-    //'resources/lib/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
-    'node_modules/sweetalert2/dist/sweetalert2.min.css',
-    //'node_modules/admin-lte/dist/css/adminlte.min.css',
-    'public/lib/rtl/adminlte/adminlte.css',
-    'public/lib/rtl/adminlte/custom.css'
-    //'resources/lib/summernote/summernote-bs4.min.css'
-],'public/css/admin/app.css')
-    .version();
 
-mix.scripts([
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/popperjs/dist/umd/popper.min.js',
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    'node_modules/@fortawesome/fontawesome-free/js/all.min.js',
-    'node_modules/sweetalert2/dist/sweetalert2.min.js',
-    'node_modules/admin-lte/dist/js/adminlte.min.js',
-    //'resources/lib/summernote/summernote-bs4.min.js'
-],'public/js/admin/app.js')
-    .version();
-
-
-
-mix.styles([
-    'node_modules/bootstrap/dist/css/bootstrap.min.css',
-    //'resources/lib/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
-    'node_modules/sweetalert2/dist/sweetalert2.min.css',
-    //'resources/lib/summernote/summernote-bs4.min.css'
-],'public/css/website/app.css')
-    .version();
-
-mix.scripts([
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/popperjs/dist/umd/popper.min.js',
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    'node_modules/@fortawesome/fontawesome-free/js/all.min.js',
-    'node_modules/sweetalert2/dist/sweetalert2.min.js',
-    //'resources/lib/summernote/summernote-bs4.min.js'
-],'public/js/website/app.js')
+mix.js('resources/js/app.js', 'public/js')
+    .react()
+    .sass('resources/sass/app.scss', 'public/css')
     .version();
